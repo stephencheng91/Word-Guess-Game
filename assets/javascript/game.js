57dd7b2289e50635a6c325b911f3787a845d2a1e
KuanToto <stephencheng91@gmail.com>
2019-03-25 20:00:51 -0700

    rev -

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,37 +9,43 @@ var guessed = document.getElementById("userGuessed");
 var guessCount = document.getElementById("guessCount");
 //Setting initial value
 var numGuess = 10;
+var charGuess = [];
+
+//Create a reset function
+function reset(){
+
+    mem = ["_ ","_ ","_ ","_ ","_ ","_ ","_ "];
+    numGuess = 10;
+    charGuess = [];
+}
  
 
 document.onkeyup = function (event){
 
-    guessed.textContent=event.key;
+    document.getElementById("guessCount").innerHTML = numGuess;
     var output ="";
-
+    var enterChar = event.key;
 
     for(var i = 0; i < answer.length; i++){
         
         if(event.key === answer[i]){
-            mem[i]=output+event.key;
-
-
-        }else if(event.key !== answer[i] && numGuess > 0){
-           numGuess-1; 
+            mem[i]=output+enterChar;
         }
-
-        document.getElementById("guessCount").innerHTML = numGuess;
-        word.textContent = mem.join("");  
-           
-            
+        word.textContent = mem.join("");
+                  
     }
 
- 
-
-    
-
- 
-
-
+    if(numGuess > 0 && !answer.includes(enterChar) && !charGuess.includes(enterChar)){
+        numGuess -= 1; 
+        charGuess.push(enterChar);
+        guessed.textContent=charGuess; 
+    }else if(numGuess>0 && mem.join("")===answer.join("")){
+        alert("Winner!!!")
+        reset();
+    }else if(numGuess === 0){
+        alert("You have loss the game");
+        alert("Game reset");
+        reset();
+    }
     
 };
-
